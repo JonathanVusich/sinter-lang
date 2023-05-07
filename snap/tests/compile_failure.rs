4825a5d589a9d56b4327d0a1794f51d27683a9e4
Jonathan Vusich <31666175+JonathanVusich@users.noreply.github.com>
2023-05-06 20:28:21 -0500

    Fix snap UI tests

--- snap/tests/compile_failure.rs
+++ snap/tests/compile_failure.rs
@@ -1,11 +1,5 @@
 #[test]
 pub fn no_specified_type() {
     let t = trybuild::TestCases::new();
-    t.compile_fail("tests/no_specified_type.rs")
-}
-
-#[test]
-pub fn not_json_serializable() {
-    let t = trybuild::TestCases::new();
-    t.compile_fail("tests/not_json_serializable.rs")
+    t.compile_fail("tests/ui/*.rs")
 }
